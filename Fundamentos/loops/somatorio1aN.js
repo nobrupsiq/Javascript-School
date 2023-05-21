24ed85090af06070b4f76753f7c92cc238afe837
bruno <bpires.siqueira@gmail.com>
2023-05-21 19:36:07 -0300

    soma de 1 a N

--- Fundamentos/loops/somatorio1aN.js
+++ Fundamentos/loops/somatorio1aN.js
@@ -3,3 +3,11 @@
 // Utiliza loops como for ou while;
 // Exiba os resultados;
 
+const n = 3;
+let soma = 0;
+
+for(let i = 1; i <= n; i++) {
+  soma += i;
+}
+
+console.log(`A soma dos números de 1 a ${n} é ${soma}`);
