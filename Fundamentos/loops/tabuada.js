8203f46a5d998c4068d93f28b7b06005ff7d6c1c
bruno <bpires.siqueira@gmail.com>
2023-05-21 23:15:44 -0300

    tabuada simples

---
+++ Fundamentos/loops/tabuada.js
@@ -0,0 +1,20 @@
+// Crie um programa que exibe a tabuada de um número;
+// Utilize as multiplicações do número 1 ao 10;
+// O exercicio deve ser feito com estruturas de repetição
+// Exiba os resultados
+
+let numero = 9
+
+// for(let i = 1; i <= 10; i++) {
+//   console.log(`${numero} x ${i} = ${numero * i}`);
+// }
+
+
+// WHILE
+let i = 1;
+let numero2 = 6
+
+while(i <= numero2) {
+  console.log(`${numero2} X ${i} = ${numero2 * i}`);
+  i++
+}
