164174fb7ae0d05eea37ee6d107dc14e4b6a832e
bruno <bpires.siqueira@gmail.com>
2023-07-30 18:59:32 -0300

    folder pilhas

---
+++ arrays/typeScriptArray.ts
@@ -0,0 +1,14 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+const friends = [
+  { name: 'Jhon', age: 30 },
+  { name: 'Ana', age: 15 },
+  { name: 'Chris', age: 25 },
+];
+
+function comparePerson(a: Person, b: Person) {
+  
+}
